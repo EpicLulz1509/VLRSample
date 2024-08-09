771f8e13d53774c2aa34fa2eab2eff01d9f57fde
A Dutta <arunopal@gmail.com>
2024-08-09 16:49:53 +0530

    commit

--- dashboard.py
+++ dashboard.py
@@ -47,7 +47,9 @@ events_str = (" ".join(events)).lower()
 
 st.subheader(f"Stats for {regions_str} region and {events_str} event")
 
-stats = specific_param_stats(events, regions)
+# stats = specific_param_stats(events, regions)
+stats = pd.read_csv('new_stats.csv')
+
 stats = stats.rename(columns={'assists' : 'ASSISTS', 'assists_per_round' : 'APR', 'average_combat_score' : 'ACS', 'average_damage_per_round' : 'ADR', 'clutch_success_percentage' : 'CS', 'deaths' : 'DEATHS', 'fd' : 'FD', 'kills' : 'KILLS', 'fk' : 'FK', 'headshot_percentage': 'HS', 'first_kills_per_round': 'FKPR', 'kill_assists_survived_traded': 'KAST', 'kill_deaths': 'KD', 'kmax': 'KMAX', 'kills_per_round': 'KPR', 'first_deaths_per_round': 'FDPR' }, errors="raise")
 # print(stats.columns, "HERE")
 stats = stats.style.set_properties(**{'font-size': '500pt'}).background_gradient(cmap='viridis').highlight_max(axis=0, subset=['rounds', 'rating', "ACS", "KD", "KAST", "ADR", "KPR", "APR",	"FKPR",	"FDPR",	"HS", "CS",	"KMAX",	"KILLS", "DEATHS", "ASSISTS", "FK",	"FD"])
